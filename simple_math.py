92e25154e3f73be964171a50c01736c631da07b6
MrAtlas <136033068+MrMime0x0@users.noreply.github.com>
2023-06-09 14:33:48 -0400

    Create simple_math.py

---
+++ simple_math.py
@@ -0,0 +1,37 @@
+import math
+
+def main():
+    """
+    This is the main function.
+    """
+    print("Hello World")
+    print("This is MrMime0x0")
+    print(math.pi)
+    print(math.e)
+    print(math.sin(math.pi))
+    print(math.cos(math.pi))
+    print(math.tan(math.pi))
+    print(math.log(math.e))
+    print(math.log10(math.e))
+    print(math.log2(math.e))
+    print(math.exp(1))
+    print(math.exp(-1))
+    print(math.exp(math.e))
+    print(math.exp(math.pi))
+    print(math.exp(math.pi/2))
+    print(math.exp(math.pi/4))
+    print(math.exp(math.pi/8))
+    print(math.exp(math.pi/16))
+    print(math.exp(math.pi/32))
+    print(math.exp(math.pi/64))
+    print(math.exp(math.pi/128))
+    print(math.exp(math.pi/256))
+    print(math.exp(math.pi/512))
+    print(math.exp(math.pi/1024))
+    print(math.exp(math.pi/2048))
+    print(math.exp(math.pi/4096))
+    print(math.exp(math.pi/8192)) 
+
+if __name__ == "__main__":
+    main()
+    print("Done")
